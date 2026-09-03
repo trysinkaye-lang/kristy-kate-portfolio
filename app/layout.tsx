bea792e9f86eac5178e1f2c7d09420885688347f
trysinkaye-lang <trysinkaye@gmail.com>
2026-09-03 17:33:54 +0800

    Import restored Contact styling last

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./feminine-editorial-v6.css";
 import "./feminine-lightmode-v7.css";
 import "./unified-editorial-v8.css";
 import "./editorial-uniform-v9.css";
+import "./contact-classic-v10.css";
 
 import { Providers } from "@/components/layout/Providers";
 import { Nav } from "@/components/layout/Nav";
