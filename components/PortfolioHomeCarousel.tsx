a15be13c3b99eb801c311b7a912597b72bd26783
trysinkaye-lang <trysinkaye@gmail.com>
2026-09-04 03:00:33 +0800

    Build recruiter-focused homepage flow

--- components/PortfolioHomeCarousel.tsx
+++ components/PortfolioHomeCarousel.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import { BlueprintHero } from "@/components/BlueprintHero";
+import { SelectedWork } from "@/components/home/SelectedWork";
+import { HomeCapabilities } from "@/components/home/HomeCapabilities";
+import { HomeAbout } from "@/components/home/HomeAbout";
+import { HomeCTA } from "@/components/home/HomeCTA";
+import "@/components/home/home.css";
 
 export function PortfolioHomeCarousel() {
   return (
     <main id="main-content" className="portfolio-v2">
       <BlueprintHero />
+      <SelectedWork />
+      <HomeCapabilities />
+      <HomeAbout />
+      <HomeCTA />
     </main>
   );
 }
