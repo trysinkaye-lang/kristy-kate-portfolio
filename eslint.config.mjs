fa7035a43d8e861f95455a2254d70523ccf19799
trysinkaye-lang <trysinkaye@gmail.com>
2026-09-03 11:55:29 +0800

    Align lint rules with legacy React Bits and R3F patterns

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,25 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-export default defineConfig([...nextVitals, ...nextTs, globalIgnores([".next/**", "out/**"])]);
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    files: [
+      "components/react-bits/DepthCarousel.jsx",
+      "components/react-bits/MaskedHeading.jsx",
+      "components/react-bits/ScrollExpand.jsx",
+    ],
+    rules: {
+      "react-hooks/refs": "off",
+    },
+  },
+  {
+    files: ["components/three/**/*.tsx"],
+    rules: {
+      "react-hooks/immutability": "off",
+    },
+  },
+  globalIgnores([".next/**", "out/**"]),
+]);
