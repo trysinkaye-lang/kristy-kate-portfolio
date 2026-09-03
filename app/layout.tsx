dd00d6d06c80cffbd3bfabb5b0024e2dfec3a063
trysinkaye-lang <trysinkaye@gmail.com>
2026-09-03 13:15:53 +0800

    Activate portrait-free Home system board

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ import "./cinematic-details-contact-v12.css";
 import "./warm-light-refinement-v12.css";
 import "./home-blueprint-image-quality-v13.css";
 import "./home-editorial-v14.css";
+import "./home-system-board-v15.css";
 
 import { Providers } from "@/components/layout/Providers";
 import { Nav } from "@/components/layout/Nav";
