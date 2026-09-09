d18f8f21e0f37d0632adfeb74884d088fc13d231
trysinkaye-lang <trysinkaye@gmail.com>
2026-09-10 02:18:43 +0800

    stage homepage flow cleanup

---
+++ components/PortfolioHomeCarousel.next.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { BlueprintHero } from "@/components/BlueprintHero";
+import { SelectedWork } from "@/components/home/SelectedWork";
+import { WebsiteDesignWork } from "@/components/home/WebsiteDesignWork";
+import { HomeCapabilities } from "@/components/home/HomeCapabilities";
+import { HomeAbout } from "@/components/home/HomeAbout";
+import { HomeCTA } from "@/components/home/HomeCTA";
+import "@/components/home/home.css";
+import "@/components/home/creative-2026.css";
+import "@/components/home/professional-home.css";
+
+export function PortfolioHomeCarousel() {
+  return (
+    <main id="main-content" className="portfolio-v2">
+      <BlueprintHero />
+      <SelectedWork />
+      <WebsiteDesignWork />
+      <HomeCapabilities />
+      <HomeAbout />
+      <HomeCTA />
+    </main>
+  );
+}
