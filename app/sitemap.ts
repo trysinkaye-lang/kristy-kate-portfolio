d89206c8f9e4aa66a3cee746ad9b11dabc65a5d4
trysinkaye-lang <trysinkaye@gmail.com>
2026-09-11 02:25:41 +0800

    Include packages in portfolio sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,10 @@
 import type { MetadataRoute } from "next";
 import { projects } from "@/data/projects";
 import { site } from "@/data/site";
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  return ["", "/projects", "/about", "/contact", ...projects.map(project => `/projects/${project.slug}`)].map(path => ({ url: `${site.url}${path}` }));
+  const staticRoutes = ["", "/projects", "/about", "/packages", "/contact"];
+  return [...staticRoutes, ...projects.map((project) => `/projects/${project.slug}`)].map((path) => ({
+    url: `${site.url}${path}`,
+  }));
 }
