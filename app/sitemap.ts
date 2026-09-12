63778bf07ea079c6490f81e2486c76b679054c29
trysinkaye-lang <trysinkaye@gmail.com>
2026-09-12 21:53:39 +0800

    Remove Lacomus from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,9 @@ import { site } from "@/data/site";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const staticRoutes = ["", "/projects", "/about", "/packages", "/contact"];
-  return [...staticRoutes, ...projects.map((project) => `/projects/${project.slug}`)].map((path) => ({
+  const visibleProjects = projects.filter(project => project.slug !== "lacomus");
+
+  return [...staticRoutes, ...visibleProjects.map(project => `/projects/${project.slug}`)].map(path => ({
     url: `${site.url}${path}`,
   }));
 }
